b7cd4236a75621b453745cf58826dbc882e85eb7
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-15 15:10:58 +0300

    generate login token

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,7 +7,7 @@ router.post('/', (req, res) => {
     const { credentials } = req.body;
     User.findOne({ email: credentials.email }).then(user => {
         if(user && user.isValidPassword(credentials.password)){
-            res.json({ user: { email: user.email } })
+            res.json({ user: user.toAuthJSON() })
         }else{
             res.status(400).json({ errors: { global: "invalid credentials" } });
         }
